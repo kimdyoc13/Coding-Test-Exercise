dad8873dbf476fd311e2a00b48496e1e0cbe7918
DONGYEONG KIM <116950743+kimdyoc13@users.noreply.github.com>
2025-12-19 14:57:53 +0900

    [Bronze III] Title: 직각삼각형, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/4153. 직각삼각형/직각삼각형.py
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.py
@@ -1,14 +1,9 @@
-while 1:
-  a, b, c = map(int, input().split())
-  if a == 0 and b == 0 and c == 0:
-    break;
-  
-  if max(a, b, c) == a:
-    a, c = c, a
-  elif max(a, b, c) == b:
-    b, c = c, b
-  
-  if a*a + b*b == c*c:
-    print("right")
-  else:
-    print("wrong")
+while True: # ~~ 가 나올때까지
+    num_list = list(map(int, input().split()))
+    num_list.sort()
+    if num_list[0] ==0:
+        break
+    elif num_list[0]**2 + num_list[1]**2 == num_list[2]**2:
+        print("right")
+    elif num_list[0]**2 + num_list[1]**2 != num_list[2]**2:
+        print("wrong")
